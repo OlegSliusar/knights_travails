762cb17bd6f9937341b17599f0c0858257e02b37
Oleh Sliusar <olehsliusar@gmail.com>
2017-04-11 11:16:27 +0300

    Complete knight_moves method

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,3 +1,13 @@
 def knight_moves(initial, target)
+  require_relative 'knight'
+  knight = Knight.new
+  path = knight.find_path(initial, target)
+  output(path)
+  path
+end
 
+def output(path)
+  puts "You made it in #{path.length - 1} moves. Here's your path:"
+  path.each { |move| p move}
+  puts ""
 end
