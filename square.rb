8c973bb4598e9d48741409dcb653bf74a2ca6abf
Oleh Sliusar <olehsliusar@gmail.com>
2017-04-11 08:06:43 +0300

    Create Square object

---
+++ square.rb
@@ -0,0 +1,8 @@
+class Square
+  attr_accessor :row, :column, :distance, :predecessor, :neighbors
+  def initialize(row, column)
+    @row = row
+    @column = column
+    @neighbors = []
+  end
+end
